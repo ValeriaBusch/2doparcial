2b3faae857d6a8d23c1ce74680b9613cd7881fac
visual_community@outlook.com <visual_community@outlook.com>
2019-11-30 15:26:57 -0300

    2 CMMIT

--- Busch/UnitTest/UnitTest1.cs
+++ Busch/UnitTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Busch;
 
 namespace UnitTest
 {
@@ -7,22 +8,39 @@ namespace UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestUnitarioAHORRO()
         {
-            double inicialProject = 500;
-            double saved = 100;
-            double valoresperado = 600;
-            Company person = new Company("Hola",inicialproject);
+            //Organizar
+            string Nombre = "TestUnitarioAHORRO";
+            double Monto= 500;
+            double Montoutilizado = 400;
+            double valoresperado = 100;
+            Company ObjetoPrueba = new Company("HOLA", Monto);
 
             //Actuar
 
-            person.Save(saved);
+             ObjetoPrueba.Save(Montoutilizado);
 
             //Afirmar
-            double valorresultado = person.SaveProject;
+            double valorresultado = ObjetoPrueba.SaveProject;
             Assert.AreEqual(valoresperado, valorresultado);
         }
+        public void TestUnitarioNombre()
+        {
+            //Organizar
+            string NombreInicial = "Busch";
+            string NombreNuevo = "Valeria";
+            string valoresperado = "Valeria";
+            Company person = new Company(NombreInicial, 100);
 
+            //Actuar
+
+            person.ChangeName(NombreNuevo);
+
+            //Afirmar
+            string valorresultado = person.Name;
+            Assert.AreEqual(valoresperado, valorresultado);
+        }
     }
 }
 }
